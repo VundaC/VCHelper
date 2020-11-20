c48427a1cebffda402eb7b1bb8a5371978c0acec
Anton Dashivskyi <Vundada@gmail.com>
2020-11-20 16:56:29 +0200

    add api to get images.

--- TempSolutions/TestIdea/TestIdea/Program.cs
+++ TempSolutions/TestIdea/TestIdea/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace TestIdea
 {
 	class Program
 	{
+		//https://pixabay.com/api/docs/
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			LoadImages().Wait();
+		}
+
+		private static async Task LoadImages()
+		{
+			try
+			{
+				var assembly = typeof(Program).Assembly;
+
+				var resource = assembly.GetManifestResourceNames();
+				using var mStream = assembly.GetManifestResourceStream("TestIdea.keys.json");
+				using var reader = new StreamReader(mStream);
+				var json = reader.ReadToEnd();
+				var jObj = JObject.Parse(json);
+				var key = jObj["pixabay_key"].ToString();
+				var client = new HttpClient();
+				var request = new HttpRequestMessage(HttpMethod.Get, $"https://pixabay.com/api/?key={key}&per_page=3");
+				var response = await client.SendAsync(request);
+				var content = await response.Content.ReadAsStringAsync();
+				Console.WriteLine(content);
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.WriteLine($"EXCEPTION LoadImages() : {ex.Message}\n{ex}");
+			}
 		}
 	}
 }

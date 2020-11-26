009ae0cbabec1db029ec9934e65da081efd75088
roman_tabachuk <romant@vundac.com>
2020-11-26 12:09:30 +0200

    Added Blazor page

--- VCHelper/VCHelper/VCHelper/App.xaml.cs
+++ VCHelper/VCHelper/VCHelper/App.xaml.cs
@@ -1,16 +1,30 @@
-﻿using Xamarin.Forms;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.MobileBlazorBindings;
+using Xamarin.Forms;
 
 namespace VCHelper
 {
     public partial class App : Application
     {
+        public static IHost Host { get; private set; }
 
-        public App()
+        public App(IFileProvider fileProvider = null)
         {
             InitializeComponent();
 
             //Register services here
             //DependencyService.Register<MockDataStore>();
+
+            Host = MobileBlazorBindingsHost.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddBlazorHybrid();
+                })
+                .UseWebRoot("wwwroot")
+                .UseStaticFiles(fileProvider)
+                .Build();
 
             MainPage = new AppShell();
         }
---
+++ VCHelper/VCHelper/VCHelper/Views/BlazorPage.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.MobileBlazorBindings;
+using VCHelper.Razor;
+using Xamarin.Forms;
+
+namespace VCHelper.Views
+{
+	public class BlazorPage : ContentPage
+	{
+		public BlazorPage()
+		{
+			App.Host.AddComponent<Main>(this);
+		}
+	}
+}

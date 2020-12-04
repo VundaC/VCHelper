1f4384873f9c4581327fcfdaf4d30ba6ad46e530
Alex <31686881+Midnayt@users.noreply.github.com>
2020-12-04 10:19:03 +0200

    Clean Up ignored files

--- VCHelper/VCHelper/VCHelper/AppShell.xaml.cs
+++ VCHelper/VCHelper/VCHelper/AppShell.xaml.cs
@@ -6,13 +6,12 @@ using Xamarin.Forms;
 
 namespace VCHelper
 {
-    public partial class AppShell : Xamarin.Forms.Shell
+    public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
-            Routing.RegisterRoute(nameof(BlazorPage), typeof(BlazorPage));
+            
         }
     }
 }

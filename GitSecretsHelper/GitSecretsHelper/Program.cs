66aaeddc06d654e8c0f1df681d052b96ce360023
Anton Dashivskyi <Vundada@gmail.com>
2020-12-04 12:00:12 +0200

    Add encryption

--- GitSecretsHelper/GitSecretsHelper/Program.cs
+++ GitSecretsHelper/GitSecretsHelper/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -13,7 +14,7 @@ namespace GitSecretsHelper
 
 		static void Main(string[] args)
 		{
-			DecodeFilesAsync().Wait();
+			EncodeFileAsync().Wait();
 		}
 
 		private static async Task DecodeFilesAsync()
@@ -35,6 +36,56 @@ namespace GitSecretsHelper
 					path = relative + path;
 #endif
 						Process.Start("cmd", $"/c \" gpg --output {path} --decrypt {path}.secret \"");
+					}
+				}
+
+				Console.WriteLine("finish");
+				Console.ReadLine();
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.WriteLine($"EXCEPTION DecodeFilesAsync() : {ex.Message}\n{ex}");
+				Console.WriteLine($"EXCEPTION: {ex.Message}\n{ex}");
+				Console.ReadLine();
+			}
+		}
+
+
+		private static async Task EncodeFileAsync()
+		{
+			try
+			{
+				var path_to_mapping = ".gitsecret/paths/mapping.cfg";
+#if DEBUG
+				path_to_mapping = relative + path_to_mapping;
+#endif
+
+
+				var assembly = typeof(Program).Assembly;
+				// json file contains emails property with array of emails for recipients
+				using var emailsstrem = assembly.GetManifestResourceStream("GitSecretsHelper.encrypt_emails.json");
+				using var reader = new StreamReader(emailsstrem);
+				var emailsstr = await reader.ReadToEndAsync();
+				var jobj = JObject.Parse(emailsstr);
+				var emails = jobj["emails"].ToObject<List<string>>();
+				var strrecipients = "";
+				foreach (var email in emails)
+					strrecipients += $" --recipient {email} ";
+
+				var files = await File.ReadAllLinesAsync(path_to_mapping);
+				if (files?.Any() == true)
+				{
+					foreach (var filePath in files)
+					{
+						var path = filePath.Substring(0, filePath.IndexOf(":"));
+						Console.WriteLine(path);
+#if DEBUG
+						path = relative + path;
+#endif
+						if (File.Exists($"{path}.secret"))
+							File.Delete($"{path}.secret");
+						var command = $"/c \" gpg --output {path}.secret --encrypt {strrecipients} {path} \"";
+						Process.Start("cmd", command);
 					}
 				}
 

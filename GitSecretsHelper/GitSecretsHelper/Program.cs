c3e619948fbad62a61ed2ad3ed8b9bd647a3d3c2
Anton Dashivskyi <Vundada@gmail.com>
2020-12-09 16:30:33 +0200

    GitSecretsHelper

--- GitSecretsHelper/GitSecretsHelper/Program.cs
+++ GitSecretsHelper/GitSecretsHelper/Program.cs
@@ -10,37 +10,44 @@ namespace GitSecretsHelper
 {
 	class Program
 	{
-		private const string relative = "../../../../../";
-
 		static void Main(string[] args)
 		{
-			var command = Console.ReadLine();
-			switch (command)
+			try
 			{
-				case "encrypt":
-				case "hide": EncodeFilesAsync().Wait(); break;
-				case "decode":
-				case "reveal" : DecodeFilesAsync().Wait(); break;
-				default: break;
-			}
-			if (command.StartsWith("addfile "))
+				var command = Console.ReadLine();
+				switch (command)
+				{
+					case "encrypt":
+					case "hide": EncodeFilesAsync().Wait(); break;
+					case "decode":
+					case "reveal": DecodeFilesAsync().Wait(); break;
+					default: {
+							if (command.StartsWith("addfile "))
+							{
+								var file = command.Replace("addfile ", "").Replace("add ", "");
+								AddFiledToSecretsAsync(file).Wait();
+							}
+							else
+							{
+								Console.WriteLine("Command not correct");
+							}
+						} break;
+				}
+				Console.ReadLine();
+			} catch (Exception ex)
 			{
-				//addfile to mapping.cfg.
-				//addfile to gitignore;
+				Console.WriteLine($"Excpetion: {ex.Message}'\n{ex}");
+				Console.ReadLine();
 			}
-#if DEBUG
-			Console.ReadLine();
-#endif
 		}
 
 		private static async Task DecodeFilesAsync()
 		{
 			try
 			{
-				var path_to_mapping = ".gitsecret/paths/mapping.cfg";
-#if DEBUG
-			path_to_mapping = relative + path_to_mapping;
-#endif
+				var path_to_mapping = ".gitsecret/paths/mapping.cfg".GetRelative();
+				Console.WriteLine(path_to_mapping);
+				Console.ReadLine();
 				var files = await File.ReadAllLinesAsync(path_to_mapping);
 				if (files?.Any() == true)
 				{
@@ -48,9 +55,8 @@ namespace GitSecretsHelper
 					{
 						var path = filePath.Substring(0, filePath.IndexOf(":"));
 						Console.WriteLine(path);
-#if DEBUG
-						path = relative + path;
-#endif
+						path = path.GetRelative();
+
 						if (File.Exists(path))
 							File.Delete(path);
 						Process.Start("cmd", $"/c \" gpg --output {path} --decrypt {path}.secret \"");
@@ -71,17 +77,18 @@ namespace GitSecretsHelper
 		{
 			try
 			{
-				var path_to_mapping = ".gitsecret/paths/mapping.cfg";
-#if DEBUG
-				path_to_mapping = relative + path_to_mapping;
-#endif
-
+				var path_to_mapping = ".gitsecret/paths/mapping.cfg".GetRelative();
 
 				var assembly = typeof(Program).Assembly;
 				// json file contains emails property with array of emails for recipients
-				using var emailsstrem = assembly.GetManifestResourceStream("GitSecretsHelper.encrypt_emails.json");
-				using var reader = new StreamReader(emailsstrem);
-				var emailsstr = await reader.ReadToEndAsync();
+				var email_path = ".gitsecret/encrypt_emails.json";
+				if(!File.Exists(email_path))
+				{
+					Console.WriteLine($"Cannot find file: {email_path}");
+					Console.ReadLine();
+					return;
+				}
+				var emailsstr = await File.ReadAllTextAsync(email_path);
 				var jobj = JObject.Parse(emailsstr);
 				var emails = jobj["emails"].ToObject<List<string>>();
 				var strrecipients = "";
@@ -95,9 +102,8 @@ namespace GitSecretsHelper
 					{
 						var path = filePath.Substring(0, filePath.IndexOf(":"));
 						Console.WriteLine(path);
-#if DEBUG
-						path = relative + path;
-#endif
+						path = path.GetRelative();
+
 						if (File.Exists($"{path}.secret"))
 							File.Delete($"{path}.secret");
 						var command = $"/c \" gpg --output {path}.secret --encrypt {strrecipients} {path} \"";
@@ -113,9 +119,41 @@ namespace GitSecretsHelper
 			}
 		}
 
-		private static async Task AddFiledToSecretsAsync()
+		private static async Task AddFiledToSecretsAsync(string file)
 		{
 
-		} 
+			if (!File.Exists(file)) throw new ArgumentException("filename");
+
+			var path_to_mapping = ".gitsecret/paths/mapping.cfg".GetRelative();
+
+			var files = await File.ReadAllLinesAsync(path_to_mapping);
+			Console.WriteLine($"Already in secret {files.Length} files");
+			if (files.FirstOrDefault(x => x.StartsWith(file)) != null)
+				throw new ArgumentException("file already added");
+			await File.AppendAllLinesAsync(path_to_mapping, new[] { $"{file}:{Guid.NewGuid()}" });
+
+			var gitignorepath = ".gitignore".GetRelative();
+			if (!File.Exists(gitignorepath))
+			{
+				Console.WriteLine("Create .gitignore");
+				File.Create(gitignorepath);
+			};
+			await File.AppendAllLinesAsync(gitignorepath, new[] { file });
+			Console.WriteLine("Append file to .gitignore");
+
+			var command = $"/c \" git rm --cahced {file} \"";
+		}
+	}
+
+
+	public static class PathExtenstions
+	{
+		public static string GetRelative(this string path)
+		{
+#if DEBUG
+			return "../../../../../" + path;
+#endif
+			return path;
+		}
 	}
 }

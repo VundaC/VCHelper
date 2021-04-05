15e7eda110bf1c29ee39a672bdaa7a9f8ecf3857
Vunda <dam@vundac.com>
2021-04-05 15:16:09 +0300

    Validation base
    Responsibility chains pattern

---
+++ VCHelper/VCHelper/VCHelper/Validation/BaseValidation.cs
@@ -0,0 +1,46 @@
+﻿using VCHelper.Validation.Exceptions;
+
+namespace VCHelper.Validation
+{
+	public abstract class BaseValidation : IValidation
+	{
+		private BaseValidation _nextValidation;
+
+		protected abstract string _reason { get; }
+		protected string _externalReason;
+
+		public void SetReason(string reason)
+		{
+			_externalReason = reason;
+		}
+
+		public void AddValidation(BaseValidation validation)
+		{
+			if (_nextValidation == null)
+				_nextValidation = validation;
+			else
+				_nextValidation.AddValidation(validation);
+		}
+
+		public void InservValidatioin(BaseValidation validation)
+		{
+			if (validation == null) return;
+			validation.AddValidation(_nextValidation);
+			_nextValidation = validation;
+		}
+
+		public void Validate(string s)
+		{
+			InternalValidate(s);
+			_nextValidation?.Validate(s);
+		}
+
+		protected abstract void InternalValidate(string s);
+
+		protected virtual void ThrowValidate()
+		{
+			var reason = string.IsNullOrWhiteSpace(_externalReason) ? _reason : _externalReason;
+			throw new ValidateException(this, reason);
+		}
+	}
+}
---
+++ VCHelper/VCHelper/VCHelper/Validation/NotNullOrEmptyValidation.cs
@@ -0,0 +1,12 @@
+﻿namespace VCHelper.Validation
+{
+	public class NotNullOrEmptyValidation : BaseValidation
+	{
+		protected override string _reason => "Value cannot be empty";
+
+		protected override void InternalValidate(string s)
+		{
+			if (string.IsNullOrWhiteSpace(s)) ThrowValidate();
+		}
+	}
+}
